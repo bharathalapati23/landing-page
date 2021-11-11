3308f9216bf302b70e2eefc9eef2b82eb4e9fe27
Vishnu Vardhan Reddy <cvishnureddy5500@gmail.com>
2021-11-11 19:16:20 +0530

    added responsiveness to the websiite

--- src/components/ContactUsComponent/ContactUsComponent.js
+++ src/components/ContactUsComponent/ContactUsComponent.js
@@ -1,60 +1,121 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import PhoneIcon from '@material-ui/icons/Phone';
-import EmailIcon from '@material-ui/icons/Email';
-import IconButton from '@material-ui/core/IconButton';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import PhoneIcon from "@material-ui/icons/Phone";
+import EmailIcon from "@material-ui/icons/Email";
+import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from "@material-ui/icons/Forum";
+import ContactForm from "./ContactForm";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        maxWidth: '1200px',
-        margin: '0 auto',
-        marginTop: '30px',
+  root: {
+    width: "100%",
+    margin: "0 auto",
+    marginTop: "30px",
+    fontFamily: "poppins",
+    [theme.breakpoints.between("xs", "sm")]: {
+      marginBottom: "15vh",
     },
-    heading: {
-        fontSize: '40px',
-        fontWeight: 'bolder',
-        marginTop: '40px'
+  },
+  heading: {
+    fontWeight: "bolder",
+    marginTop: "40px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    [theme.breakpoints.up("xs")]: {
+      fontSize: "20px",
     },
-    deleteIcon1: {
-        '& svg': {
-            fontSize: 50,
-            color: 'black'
-        }
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "30px",
     },
-    dottedLine: {
-        border: 'none',
-        borderTop: '2px dashed grey',
+    [theme.breakpoints.up("md")]: {
+      fontSize: "35px",
     },
+    marginBottom: "3vh",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: "5vh",
+    marginBottom: "5vh",
+  },
+  contactRow: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "2vh",
+  },
+  text: {
+    fontSize: "20px",
+    [theme.breakpoints.between("xs", "sm")]: {
+      fontSize: "15px",
+    },
+    [theme.breakpoints.between("sm", "md")]: {
+      fontSize: "18px",
+    },
+  },
+  contactIcon: {
+    marginRight: "2vw",
+    [theme.breakpoints.up("xs")]: {
+      fontSize: 25,
+    },
+    [theme.breakpoints.up("sm")]: {
+      fontSize: 30,
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: 35,
+    },
+    color: "black",
+  },
+  Icon: {
+    fontSize: 30,
+    [theme.breakpoints.between("xs", "sm")]: {
+      fontSize: 20,
+    },
+    [theme.breakpoints.between("sm", "md")]: {
+      fontSize: 25,
+    },
+    color: "black",
+  },
+  dottedLine: {
+    border: "none",
+    borderTop: "2px dashed grey",
+  },
 }));
 
-
 const ContactUsComponent = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <div className={classes.heading}>
-                Contact Us
-            </div>
-            <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'space-around', fontSize: '20px', alignItems: 'center' }}>
-                <IconButton className={classes.deleteIcon1} >
-                    <PhoneIcon />
-                </IconButton>
-                <div>
-                    +91 95138 69695
-                </div>
-            </div>
-            <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'space-around', fontSize: '20px', alignItems: 'center' }}>
-                <IconButton className={classes.deleteIcon1} >
-                    <EmailIcon />
-                </IconButton>
-                <div>
-                    info@7thheavenhomes.com
-                </div>
-            </div>
-            <hr className={classes.dottedLine}></hr>
+  return (
+    <div className={classes.root}>
+      <div>
+        <div className={classes.heading}>
+          <ForumIcon className={classes.contactIcon} />
+          Contact Us
         </div>
-    )
-}
+        <div className={classes.form}>
+          <ContactForm />
+        </div>
+        <hr className={classes.dottedLine}></hr>
+        <div className={classes.contactRow}>
+          <IconButton>
+            <PhoneIcon className={classes.Icon} />
+          </IconButton>
+          <div className={classes.text}>+91 95138 69695</div>
+        </div>
+        <div className={classes.contactRow}>
+          <IconButton>
+            <EmailIcon className={classes.Icon} />
+          </IconButton>
+          <div className={classes.text}>info@7thheavenhomes.com</div>
+        </div>
+        <hr className={classes.dottedLine}></hr>
+      </div>
+    </div>
+  );
+};
 
-export default ContactUsComponent
+export default ContactUsComponent;
--- src/components/PricingComponent/PricingComponent.js
+++ src/components/PricingComponent/PricingComponent.js
@@ -1,79 +1,249 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import PriceCard from './PriceCard'
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { makeStyles, createTheme } from "@material-ui/core/styles";
+import PriceCard from "./PriceCard";
+import Button from "@material-ui/core/Button";
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+    },
+  },
+});
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        fontFamily: 'poppins',
-        fontSize: '20px',
-        maxWidth: '1200px',
-        margin: '0 auto',
-        marginTop: '30px',
-
+  root: {
+    fontFamily: "poppins",
+    width: "100%",
+  },
+  priceCardRoot: {
+    display: "flex",
+    marginTop: "5vh",
+    marginBottom: "5vh",
+    [theme.breakpoints.up("xs")]: {
+      flexDirection: "column",
+      justifyContent: "center",
+      width: "100%",
+      alignItems: "center",
     },
-    priceCardRoot: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around'
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "row",
+      justifyContent: "space-around",
+      width: "80%",
+      alignItems: "center",
     },
-    card: {
-        width: '30%'
+  },
+  cards: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heading: {
+    fontWeight: "bolder",
+    marginTop: "40px",
+    [theme.breakpoints.up("xs")]: {
+      fontSize: "20px",
     },
-    heading: {
-        fontSize: '40px',
-        fontWeight: 'bolder',
-        marginTop: '40px'
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "30px",
     },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "35px",
+    },
+  },
+  plans: {
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    [theme.breakpoints.up("xs")]: {
+      flexDirection: "column",
+    },
+    [theme.breakpoints.up("sm")]: {
+      flexDirection: "row",
+    },
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "row",
+      marginTop: "2vh",
+      marginBottom: "2vh",
+    },
+  },
+  planImage: {
+    [theme.breakpoints.up("xs")]: {
+      height: "50vh",
+      width: "80vw",
+      marginTop: "3vh",
+    },
+    [theme.breakpoints.up("sm")]: {
+      height: "50vh",
+      width: "40vw",
+      marginTop: "3vh",
+    },
+    [theme.breakpoints.up("md")]: {
+      height: "60vh",
+      width: "40vw",
+      marginTop: "3vh",
+    },
+  },
+  mPlan: {
+    marginTop: "5vh",
+    marginBottom: "5vh",
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mPlanImage: {
+    [theme.breakpoints.up("xs")]: {
+      width: "80vw",
+      height: "60vh",
+    },
+    [theme.breakpoints.up("sm")]: {
+      width: "60vw",
+      height: "60vh",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "auto",
+      height: "80vh",
+    },
+  },
+  planIllustrations: {
+    marginTop: "3vh",
+    marginBottom: "3vh",
+    display: "flex",
+    [theme.breakpoints.up("xs")]: {
+      flexDirection: "column",
+    },
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "row",
+      width: "100%",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  },
+  illImage: {
+    margin: "10px",
+    [theme.breakpoints.between("xs", "sm")]: {
+      width: "40vw",
+    },
+  },
+  planCol1: {
+    display: "flex",
+    flexDirection: "row",
+    [theme.breakpoints.between("xs", "sm")]: {
+      width: "100%",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  },
+  planCol2: {
+    marginTop: "10px",
+    marginBottom: "10px",
+    [theme.breakpoints.between("xs", "sm")]: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  },
+  illImage2: {
+    [theme.breakpoints.between("xs", "sm")]: {
+      width: "83vw",
+    },
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: "8px",
+    },
+  },
 }));
 
-
 const PricingComponent = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <>
-            <div className={classes.root}>
-                <div className={classes.priceCardRoot}>
-                    <PriceCard />
-                    <PriceCard />
-                    <PriceCard />
-                </div>
+  return (
+    <>
+      <div className={classes.root}>
+        <div className={classes.cards}>
+          <div className={classes.priceCardRoot}>
+            <PriceCard />
+            <PriceCard />
+            <PriceCard />
+          </div>
+        </div>
 
-                <div className={classes.heading}>
-                    Book A FREE Site-Visit to G Corp Residences
-                </div>
-                <Button variant="contained" disableElevation style={{ marginTop: '30px' }}>
-                    Schedule My Site-Visit
-                </Button>
-                <div className={classes.heading}>
-                    G Corp Resdiences Plans
-                </div>
-                <div style={{ marginTop: '40px', display: 'flex', justifyContent: 'space-evenly' }}>
-                    <img src="https://images.assets-landingi.com/aabSDaqctafEAWJ0/floor_plan_1.jpg"></img>
-                    <img src="https://images.assets-landingi.com/aabSDaqctafEAWJ0/floor_plan_1.jpg"></img>
-                </div>
-                <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'space-evenly' }}>
-                    <img src="https://images.assets-landingi.com/aabSDaqctafEAWJ0/floor_plan_1.jpg"></img>
-                    <img src="https://images.assets-landingi.com/aabSDaqctafEAWJ0/floor_plan_1.jpg"></img>
-                </div>
-                <div className={classes.heading}>
-                    G Corp Resdiences Master Plan
-                </div>
-                <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'space-evenly' }}>
-                    <img src="https://images.assets-landingi.com/VznT4k756OPf3wml/master_plan.jpg"></img>
-                </div>
-                <div className={classes.heading}>
-                    Prestige Pinewood Images
-                </div>
-                <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'center' }}>
-                    <img src="https://images.assets-landingi.com/qhO51eQ5gZAHPvnY/gallery_1.jpg" style={{ marginRight: '20px' }}></img>
-                    <img src="https://images.assets-landingi.com/prUzIlrJlnS0eWNM/gallery_2.jpg" style={{ marginRight: '20px' }}></img>
-                    <img src="https://images.assets-landingi.com/j6i7xrRgSxhVb2Zl/gallery_3.jpg"></img>
-                </div>
-            </div>
-        </>
-    )
-}
+        <div className={classes.heading}>
+          Book A FREE Site-Visit to G Corp Residences
+        </div>
+        <Button
+          variant="contained"
+          disableElevation
+          style={{ marginTop: "30px" }}
+        >
+          Schedule My Site-Visit
+        </Button>
+        <div className={classes.heading}>G Corp Resdiences Plans</div>
+        <div className={classes.plans}>
+          <img
+            src="https://images.assets-landingi.com/aabSDaqctafEAWJ0/floor_plan_1.jpg"
+            alt="assetPlan"
+            className={classes.planImage}
+          />
+          <img
+            src="https://images.assets-landingi.com/aabSDaqctafEAWJ0/floor_plan_1.jpg"
+            alt="assetPlan"
+            className={classes.planImage}
+          />
+        </div>
+        <div className={classes.plans}>
+          <img
+            src="https://images.assets-landingi.com/aabSDaqctafEAWJ0/floor_plan_1.jpg"
+            alt="assetPlan"
+            className={classes.planImage}
+          />
+          <img
+            src="https://images.assets-landingi.com/aabSDaqctafEAWJ0/floor_plan_1.jpg"
+            alt="assetPlan"
+            className={classes.planImage}
+          />
+        </div>
+        <div className={classes.heading}>G Corp Resdiences Master Plan</div>
+        <div className={classes.mPlan}>
+          <img
+            className={classes.mPlanImage}
+            src="https://images.assets-landingi.com/VznT4k756OPf3wml/master_plan.jpg"
+            alt="mastePlanImage"
+          />
+        </div>
+        <div className={classes.heading}>Prestige Pinewood Images</div>
+        <div className={classes.planIllustrations}>
+          <div className={classes.planCol1}>
+            <img
+              src="https://images.assets-landingi.com/qhO51eQ5gZAHPvnY/gallery_1.jpg"
+              alt="planIllustration"
+              className={classes.illImage}
+            />
+            <img
+              src="https://images.assets-landingi.com/prUzIlrJlnS0eWNM/gallery_2.jpg"
+              alt="planIllustration"
+              className={classes.illImage}
+            />
+          </div>
+          <div className={classes.planCol2}>
+            <img
+              src="https://images.assets-landingi.com/j6i7xrRgSxhVb2Zl/gallery_3.jpg"
+              alt="planIllustration"
+              className={classes.illImage2}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default PricingComponent
+export default PricingComponent;
